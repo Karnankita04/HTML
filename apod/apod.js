faae89202cd3eb987481af16509fa03f2315bdc0
Karnankita04 <91003709+Karnankita04@users.noreply.github.com>
2025-02-16 08:37:59 +0530

    updated

---
+++ apod/apod.js
@@ -0,0 +1,20 @@
+// import apod from "./apod.json" with {type: "json"};
+const about = "";
+const explanation = "";
+const image = "";
+
+const template = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>`
+
+
+
+// console.log(apod)
